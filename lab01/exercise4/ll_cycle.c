06155693f3242f803187aab30bb138801d8b515c
MFaayez <faayez.muhammad@10xengineers.ai>
2023-07-25 18:20:34 +0500

    Done exercise4

--- lab01/exercise4/ll_cycle.c
+++ lab01/exercise4/ll_cycle.c
@@ -3,4 +3,23 @@
 
 int ll_has_cycle(node *head) {
     /* TODO: Implement ll_has_cycle */
+    node* fast_ptr= head;
+    node* slow_ptr= head;
+    while(fast_ptr!=NULL)
+    {
+    	if(fast_ptr->next==NULL)
+    	{
+    		fast_ptr=fast_ptr->next;
+    	}
+    	else
+    	{	
+    		fast_ptr=fast_ptr->next;
+    		fast_ptr=fast_ptr->next;
+    	}
+    	slow_ptr=slow_ptr->next;
+    	if(fast_ptr==slow_ptr)
+    		return 1;
+    	
+    }
+    return 0;
 }
