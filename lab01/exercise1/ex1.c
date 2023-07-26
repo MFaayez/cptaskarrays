0bcc4ee89a2c672d07c7489d37584a4f6f1103a7
MFaayez <faayez.muhammad@10xengineers.ai>
2023-07-26 10:09:21 +0500

    linting done

--- lab01/exercise1/ex1.c
+++ lab01/exercise1/ex1.c
@@ -1,62 +1,44 @@
+// "Copyright 2023 <Malik Faayez Muhammad>" [legal/copyright]
 #include <string.h>
-#include<stdio.h>
-#include "ex1.h"
-
+#include <stdio.h>
+#include <ex1.h>
 /* Returns the number of times LETTER appears in STR.
 There are two different ways to iterate through a string.
 1st way hint: strlen() may be useful
 2nd way hint: all strings end in a null terminator */
 int num_occurrences(char *str, char letter) {
-    /* TODO: implement num_occurances */
-    
-    int len=strlen(str);
-    int counter=0;
-    for(int i=0; i<len; i++)
-    {
-	    if(str[i]==letter)
-		    counter++;
-    
-    }
-    
-    return counter;
+  /* TODO: implement num_occurances */
+  int len = strlen(str);
+  int counter = 0;
+  for (int i = 0; i < len; i++) {
+    if (str[i] == letter)
+      counter++;
+  }
+  return counter;
 }
 
 /* Populates DNA_SEQ with the number of times each nucleotide appears.
 Each sequence will end with a NULL terminator and will have up to 20 nucleotides.
 All letters will be upper case. */
 void compute_nucleotide_occurrences(DNA_sequence *dna_seq) {
-    /* TODO: implement compute_nucleotide_occurances */
-  
-//    DNA_sequence A=A;
-//    DNA_sequence C=C;
-    dna_seq->A_count=0;
-    dna_seq->C_count=0;
-    dna_seq->G_count=0;
-    dna_seq->T_count=0;
+  /* TODO: implement compute_nucleotide_occurances */
+  //    DNA_sequence A=A;
+  //    DNA_sequence C=C;
+  dna_seq->A_count = 0;
+  dna_seq->C_count = 0;
+  dna_seq->G_count = 0;
+  dna_seq->T_count = 0;
 
-
-
-    for(int i=0; dna_seq->sequence[i]!='\0'; i++)
-    {
-       if(dna_seq->sequence[i]=='A')
-       {
-          dna_seq->A_count+=1;
-       }
-       else if(dna_seq->sequence[i]=='C')
-       {
-          dna_seq->C_count+=1;
-       }
-       else if(dna_seq->sequence[i]=='G')
-       {
-          dna_seq->G_count+=1;
-       }
-       else if(dna_seq->sequence[i]=='T')
-       {
-         dna_seq->T_count+=1;
-       }
+  for (int i = 0; dna_seq->sequence[i] != '\0'; i++) {
+    if (dna_seq->sequence[i] == 'A') {
+      dna_seq->A_count += 1;
+    } else if (dna_seq->sequence[i] == 'C') {
+      dna_seq->C_count += 1;
+    } else if (dna_seq->sequence[i] == 'G') {
+      dna_seq->G_count += 1;
+    } else if (dna_seq->sequence[i] == 'T') {
+      dna_seq->T_count += 1;
     }
-
-    return;
+  }
+  return;
 }
-
-
